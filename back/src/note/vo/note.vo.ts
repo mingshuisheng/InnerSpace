2d097ad047eb4ad457500cf0dff5119349e11e81
mingshuisheng <mingshuisheng@qq.com>
2023-03-18 20:52:28 +0800

    修复note相关接口的json格式错误

--- back/src/note/vo/note.vo.ts
+++ back/src/note/vo/note.vo.ts
@@ -1,22 +1,5 @@
 import { Note } from '../../entity/note.entity'
 
-// export class NoteVo {
-//
-//   constructor(id: number, parentId: number,name: string, content: string = "无数据") {
-//     this.id = id
-//     this.parentId = parentId
-//     this.name = name
-//     this.children = []
-//     this.content = content
-//   }
-//
-//   id: number
-//   parentId: number
-//   name: string
-//   children?: NoteVo[]
-//   content: string
-// }
-
 export type NoteVo = {
   id: number
   parentId: number
